8bec94e9685d77351617de892b32a97634fd2962
Hongbusi <coderhbs@gmail.com>
2022-12-06 14:41:22 +0800

    chore: update

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,24 @@ export function presetHbs(): Preset {
   return {
     name: 'unocss-preset-hbs',
     shortcuts: {
+      // absolute
       'absolute-x-center': 'absolute left-1/2 -translate-x-1/2',
       'absolute-y-center': 'absolute top-1/2 -translate-y-1/2',
       'absolute-center': 'absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2',
+
+      // fixed
+      'fixed-x-center': 'fixed left-1/2 -translate-x-1/2',
+      'fixed-y-center': 'fixed top-1/2 -translate-y-1/2',
+      'fixed-center': 'fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2',
+
+      // flex
+      'flex-justify-center': 'flex justify-center',
+      'flex-items-center': 'flex items-center',
       'flex-center': 'flex justify-center items-center',
+
+      // inline-flex
+      'inline-flex-justify-center': 'inline-flex justify-center',
+      'inline-flex-items-center': 'inline-flex items-center',
       'inline-flex-center': 'inline-flex justify-center items-center'
     }
   }
